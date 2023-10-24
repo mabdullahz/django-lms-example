60261aba425a532f9d8e9c15ea51088d727402a7
Abdullah Zafar <mabdullahzafar08@gmail.com>
2023-10-24 11:26:17 +0500

    simple auth

--- student/urls.py
+++ student/urls.py
@@ -7,4 +7,7 @@ app_name = 'student'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>", views.DetailView.as_view(), name="detail"),
+    path("signup", views.sign_up, name="sign_up"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from django.views import generic
 
 from .models import Student
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 class IndexView(generic.ListView):
@@ -18,3 +22,39 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Student
+
+
+def sign_up(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('student:index')) 
+        return render(request, "student/student_signup.html")
+    elif request.method == "POST":
+        user = User.objects.create_user(
+            username=request.POST.get('username'),
+            email=request.POST.get('email'),
+            password=request.POST.get('password'),
+        )
+        return HttpResponseRedirect(reverse('student:login')) 
+
+def login_view(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('student:index')) 
+        return render(request, "student/student_login.html")
+    elif request.method == "POST":
+        username = request.POST.get('username') # POST["username"]
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('student:index')) 
+        else:
+            return render(request, "student/student_login.html", {"login_message": "Username/password is incorrect"})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('student:login')) 
