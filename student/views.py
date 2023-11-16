c2f022ffd763b68cd0bcfe65dbd1241cf8aee58c
Abdullah Zafar <mabdullahzafar08@gmail.com>
2023-11-16 11:12:58 +0500

    paginator

--- student/views.py
+++ student/views.py
@@ -12,10 +12,17 @@ from .models import Student
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+from django.core.paginator import Paginator
+
+
 # @login_required(login_url=reverse_lazy('student:login'))
 def index(request):
     if request.user.is_authenticated:
         students = Student.objects.all()
+        paginator = Paginator(students, 5)  # Show 25 contacts per page.
+
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
 
         num_visits = request.session.get('num_visits', 0)
         request.session['num_visits'] = num_visits + 1
@@ -23,6 +30,7 @@ def index(request):
         return render(request, "student/student_list.html", {
             "object_list": students,
             "session": request.session,
+            "page_obj": page_obj,
         })
     return redirect(reverse('student:login'))
 
